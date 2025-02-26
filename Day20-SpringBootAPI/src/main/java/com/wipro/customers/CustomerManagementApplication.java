80a15f7d5d3003bdbf8042400cbb8d095729d3c2
Sahil Naik <sahil7naik57@gmail.com>
2025-02-26 15:42:54 +0530

    Day20

---
+++ Day20-SpringBootAPI/src/main/java/com/wipro/customers/CustomerManagementApplication.java
@@ -0,0 +1,15 @@
+package com.wipro.customers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.wipro.customers")
+public class CustomerManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerManagementApplication.class, args);
+	}
+
+}
