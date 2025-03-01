578dfa5cf6b334f907080e99ac278f28b911eaab
Sahil Naik <sahil7naik57@gmail.com>
2025-03-01 10:51:17 +0530

    Day23

---
+++ Day23-ServerA/src/main/java/com/wipro/servera/ServerAApplication.java
@@ -0,0 +1,14 @@
+package com.wipro.servera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.*;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ServerAApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServerAApplication.class, args);
+    }
+}
---
+++ Day23-ServerB/src/main/java/com/wipro/serverb/ServerBApplication.java
@@ -0,0 +1,13 @@
+package com.wipro.serverb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ServerBApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServerBApplication.class, args);
+    }
+}
