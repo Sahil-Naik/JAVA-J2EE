b1ea8c208e1a49726d850b364c5a1c56ca4cd940
Sahil Naik <sahil7naik57@gmail.com>
2025-03-01 15:06:41 +0530

    Day23

---
+++ Day23-ServerCustomer/src/main/java/com/wipro/controller/CustomerController.java
@@ -0,0 +1,82 @@
+package com.wipro.controller;
+
+import com.wipro.DTO.CustomerDTO;
+import com.wipro.model.Customer;
+import com.wipro.service.CustomerService;
+
+import org.springframework.data.domain.Page;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/customers")
+public class CustomerController {
+	
+	@Autowired
+	private CustomerService customerService;
+	
+	@PostMapping("/add")
+	public ResponseEntity<Customer> addCustomer(@RequestBody Customer newCustomer) {
+		Customer customer = new Customer();
+		customer.setCustomerCity(newCustomer.getCustomerCity());
+		customer.setCustomerBill(newCustomer.getCustomerBill());
+	    
+		Customer savedCustomer = customerService.addCustomer(customer);
+	    return new ResponseEntity<>(savedCustomer, HttpStatus.CREATED);
+	}
+	
+	@GetMapping("/all")
+	public List<Customer> getAllCustomer(){
+		return customerService.getAllCustomers();
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<CustomerDTO> getCustomer(@PathVariable int id){
+		CustomerDTO custDTO = customerService.getCustomerById(id);
+		
+		return ResponseEntity.ok(custDTO);
+	}
+	
+	@GetMapping("/city")
+	public List<Customer> getCustomerByCity(@RequestParam String city) {
+		return customerService.getCustomerByCity(city);
+	}
+	
+	@GetMapping("/city-bill-greater-than")
+	public List<Customer> getCustomersByCityAndBillGreaterThan(
+	        @RequestParam String city, 
+	        @RequestParam double amount) {
+	    return customerService.getCustomersByCityAndBillGreaterThan(city, amount);
+	}
+	
+	@GetMapping("/city-bill-less-than")
+	public List<Customer> getCustomersByCityAndBillLessThan(
+	        @RequestParam String city, 
+	        @RequestParam double amount) {
+	    return customerService.getCustomersByCityAndBillLessThan(city, amount);
+	}
+	
+	@GetMapping("/paginate")
+	public Page<Customer> getCustomers(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "5") int size,
+            @RequestParam(defaultValue = "customerCity") String sortBy,
+            @RequestParam(defaultValue = "asc") String direction) {
+        return customerService.getCustomersPaged(page, size, sortBy, direction);
+    }
+	
+	@PutMapping("/update/{id}")
+	public Customer updateCustomer(@PathVariable int id, @RequestBody Customer updatedCustomer) {
+		return customerService.updateCustomer(id, updatedCustomer);
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public String deleteCustomer(@PathVariable int id) {
+		customerService.deleteCustomer(id);
+		return "Customer with ID " + id + " has been deleted.";
+	}
+}
