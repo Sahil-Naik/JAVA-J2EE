9e5d46d7f1694778a2e8d151e4f5c8e900ef471e
Sahil Naik <sahil7naik57@gmail.com>
2025-03-06 10:00:38 +0530

    Day27

---
+++ Day27-APIGateaway-Circuitbreaker/src/main/java/com/wipro/APIDemo.java
@@ -0,0 +1,13 @@
+package com.wipro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class APIDemo {
+
+	public static void main(String[] args) {
+		SpringApplication.run(APIDemo.class, args);
+	}
+
+}
