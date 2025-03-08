490435359ac00d694a917ec1d3951d8c49d64b51
Sahil Naik <sahil7naik57@gmail.com>
2025-03-08 19:10:38 +0530

    Final FIX maybe

--- Day27-Docker-WebClientVendor/src/main/java/com/wipro/vendor/VendorApiApplication.java
+++ Day27-Docker-WebClientVendor/src/main/java/com/wipro/vendor/VendorApiApplication.java
@@ -3,11 +3,9 @@ package com.wipro.vendor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@RefreshScope
 public class VendorApiApplication {
 
 	public static void main(String[] args) {
