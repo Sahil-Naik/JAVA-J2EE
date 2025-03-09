8ab56c6f72673dcd2d13c2d598fa6bb044ae0adf
Sahil Naik <sahil7naik57@gmail.com>
2025-03-09 11:19:58 +0530

    Day30

--- Day27-Docker-WebClientVendor/src/main/java/com/wipro/vendor/controller/MessageRestController.java
+++ Day27-Docker-WebClientVendor/src/main/java/com/wipro/vendor/controller/MessageRestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MessageRestController {
 	 
-	@Value("${spring.boot.message}")
+	@Value("Hello from main App")
 	private String message;
 	
 	@GetMapping("/message")
