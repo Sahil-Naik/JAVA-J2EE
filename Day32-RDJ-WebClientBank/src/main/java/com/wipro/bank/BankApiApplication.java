2430761006b5eb9bc4e29a72ce7cb8afb58db9a5
Sahil Naik <sahil7naik57@gmail.com>
2025-03-11 16:52:59 +0530

    Day32 - RJ

--- Day32-RDJ-WebClientBank/src/main/java/com/wipro/bank/BankApiApplication.java
+++ Day32-RDJ-WebClientBank/src/main/java/com/wipro/bank/BankApiApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
 @SpringBootApplication
 @EnableR2dbcRepositories(basePackages = "com.wipro.bank")
+@ComponentScan(basePackages = "com.wipro.bank")
 @EnableDiscoveryClient
 @RefreshScope
 public class BankApiApplication {
