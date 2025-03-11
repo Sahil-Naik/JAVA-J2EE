bff96fc3f703a02f94935edb1b1af161e4d8fa0a
Sahil Naik <sahil7naik57@gmail.com>
2025-03-11 17:39:22 +0530

    Day32 - R2DBC fix v.1.0

--- Day32-RDJ-WebClientVendor/src/main/java/com/wipro/vendor/VendorApiApplication.java
+++ Day32-RDJ-WebClientVendor/src/main/java/com/wipro/vendor/VendorApiApplication.java
@@ -3,10 +3,12 @@ package com.wipro.vendor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
 @SpringBootApplication
 @EnableR2dbcRepositories(basePackages = "com.wipro.vendor")
+@ComponentScan(basePackages = "com.wipro.vendor")
 @EnableDiscoveryClient
 public class VendorApiApplication {
 
